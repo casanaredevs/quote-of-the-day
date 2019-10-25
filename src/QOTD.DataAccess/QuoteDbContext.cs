d13bbcf7392058d4a12f1066e7350a3e4a5644ae
Juan David Pareja Soto <parejajd@outlook.com>
2019-10-25 17:08:45 -0500

    Repositorios implementados

---
+++ src/QOTD.DataAccess/QuoteDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using QOTD.Models;
+
+namespace QOTD.DataAccess
+{
+    public class QuoteDbContext : DbContext
+    {
+        public DbSet<Frase> Frases { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public QuoteDbContext(DbContextOptions<QuoteDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //EFCore por defecto pluraliza las tablas. Con esto deshabilitamos esta opción
+            foreach (IMutableEntityType entityType in builder.Model.GetEntityTypes())
+            {
+                entityType.SetTableName(entityType.DisplayName());
+            }
+        }
+    }
+}
